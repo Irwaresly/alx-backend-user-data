fbd83be3489da46499acea72d9d01cbb77c43e9f
Irwaresly <silviairware372@gmail.com>
2024-05-29 11:48:09 -0700

    009

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """A module for filtering logs.
 """
-#import os
 import re
-#import logging
-#import mysql.connector
 from typing import List
 
 
@@ -12,7 +9,7 @@ patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
-#PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 
 def filter_datum(
