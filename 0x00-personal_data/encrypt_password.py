795f3b066c8bf2053985a2585fb7b1837fcd0e8b
Irwaresly <silviairware372@gmail.com>
2024-05-29 11:57:41 -0700

    010

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,12 +5,12 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password.
+    """Hashes a password using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def is_valid(password: str, hashed_password: bytes) -> bool:
-    """Validates a password against a hashed password.
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed from the given password.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
