61101241e0c9a8a5655e8e35ca9281a75241b18c
Irwaresly <silviairware372@gmail.com>
2024-05-29 11:32:20 -0700

    007

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
+"""A module for encrypting passwords.
+"""
 import bcrypt
 
-'''
-   This function takes a password as input and returns a hashed password as a byte string.
-'''
-def hash_password(password):
-    # Generate a salt
-    salt = bcrypt.gensalt()
 
-    # Hash the password with the salt
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-    # Return the hashed password as a byte string
-    return hashed_password
+def hash_password(password: str) -> bytes:
+    """Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
