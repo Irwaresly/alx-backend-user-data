3fc8a079c918c5cae4a56e2d2ae50f9fc8a90676
Irwaresly <silviairware372@gmail.com>
2024-06-12 12:11:07 -0700

    ua19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,13 @@
 """
 import bcrypt
 
-
 def _hash_password(password: str) -> bytes:
-    """Hashes a password.
+    """Generate a salted hash of the input password.
+    
+    Args:
+        password (str): The password to be hashed.
+    
+    Returns:
+        bytes: The hashed password.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
