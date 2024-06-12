c82adad90246f7c4a702ae33743f7d5403aeca73
Irwaresly <silviairware372@gmail.com>
2024-06-12 14:24:08 -0700

    ua23

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,13 +9,18 @@ Routes:
 """
 
 from flask import Flask, jsonify
+from flask import request
+from auth import Auth
+
+# Create an instance of the Auth class
+AUTH = Auth()
 
 # Create the Flask application
 app = Flask(__name__)
 
 
 # Define a route for the root endpoint
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """Route handler for the root endpoint.
 
@@ -23,6 +28,23 @@ def index() -> str:
         dict: JSON payload with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """Route handler for the /users endpoint.
+
+    Returns:
+    dict: JSON payload with the registration status.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 # Run the Flask app if this script is executed directly
