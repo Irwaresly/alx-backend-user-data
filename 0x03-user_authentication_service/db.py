6429e75d601495538dd51181d8d61c5dcf030e39
Irwaresly <silviairware372@gmail.com>
2024-06-12 05:30:33 -0700

    au9

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
-from sqlalchemy.orm.session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class.
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """Memoized session object"""
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database.
+        """Adds a new user to the database.
         """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
