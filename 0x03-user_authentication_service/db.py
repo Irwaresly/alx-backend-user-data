1d81f0457a1a2a8babc87df3ba88439e46751743
Irwaresly <silviairware372@gmail.com>
2024-06-12 02:37:25 -0700

    ua5

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,7 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker
 from user import Base, User
 
 
@@ -15,7 +15,15 @@ class DB:
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
-        self.__session = scoped_session(sessionmaker(bind=self._engine))
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
@@ -29,13 +37,10 @@ class DB:
         """
         new_user = User(email=email, hashed_password=hashed_password)
         try:
-            self.__session.add(new_user)
-            self.__session.commit()
-            self.__session.refresh(new_user)  # Refresh the instance
+            self._session.add(new_user)
+            self._session.commit()
         except Exception as e:
-            self.__session.rollback()
+            self._session.rollback()
             new_user = None
             print(f"Error: {e}")
-        finally:
-            self.__session.remove()
         return new_user
