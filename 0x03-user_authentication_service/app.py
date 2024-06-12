e7ee6705534ad9d8d65faab6bbf35e12a29862b5
Irwaresly <silviairware372@gmail.com>
2024-06-12 13:54:15 -0700

    022ua

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,18 +1,30 @@
-#!usr/bin/env python3
-'''Module for Basic Flask App
-'''
+#!/usr/bin/env python3
+"""
+Basic Flask app with a single GET route ("/") returning a JSON payload.
+
+This script sets up a Flask app with a GET route that returns a JSON response.
+
+Routes:
+    GET '/' - Returns a JSON payload: {"message": "Bienvenue"}
+"""
 
 from flask import Flask, jsonify
 
+# Create the Flask application
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
+# Define a route for the root endpoint
+@app.route('/')
 def index() -> str:
-    '''GET method.
-    '''
+    """Route handler for the root endpoint.
+
+    Returns:
+        dict: JSON payload with a welcome message.
+    """
     return jsonify({"message": "Bienvenue"})
 
 
+# Run the Flask app if this script is executed directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
