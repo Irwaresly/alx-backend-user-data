9ad41063841f38ceb83f47aaaee146266e7b4dcd
Irwaresly <silviairware372@gmail.com>
2024-06-12 05:54:53 -0700

    au13

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database.
+        """
+        Adds a new user to the database.
+
+        Parameters:
+        email (str): User's email.
+        hashed_password (str): User's hashed password.
+
+        Returns:
+        User: The new User object if successful, otherwise None.
         """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
