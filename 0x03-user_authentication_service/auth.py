5416efe2ed49106fd2166f391e62840b6a370d25
Irwaresly <silviairware372@gmail.com>
2024-06-12 13:18:06 -0700

    ua19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,13 +2,49 @@
 """A module for authentication-related routines.
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """Generate a salted hash of the input password.
-    Args:
-        password (str): The password to be hashed.
-    Returns:
-        bytes: The hashed password.
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    def __init__(self):
+        self._db = DB()
+
+    @staticmethod
+    def hash_password(password: str) -> bytes:
+        """Generate a salted hash of the input password.
+        Args:
+            password (str): The password to be hashed.
+        Returns:
+            bytes: The hashed password.
+        """
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+
+        Parameters:
+        email (str): User's email.
+        password (str): User's password.
+
+        Returns:
+        User: The new User object if successful.
+
+        Raises:
+        ValueError: If a user with the same email already exists.
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {existing_user.email} already exists.")
+        except NoResultFound:
+            hashed_password = Auth.hash_password(password)
+            new_user = self._db.add_user(
+                email=email, hashed_password=hashed_password
+            )
+            return new_user
