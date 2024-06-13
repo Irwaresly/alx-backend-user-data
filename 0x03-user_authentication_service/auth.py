0692825dfa858c334605ac6ae255401593cf7399
Irwaresly <silviairware372@gmail.com>
2024-06-13 02:32:15 -0700

    ua27

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,24 +2,13 @@
 """A module for authentication-related routines.
 """
 import bcrypt
-from sqlalchemy.orm.exc import NoResultFound
-
-from db import DB
-from user import User
 
 
-class Auth:
-    """Auth class to interact with the authentication database.
+def _hash_password(password: str) -> bytes:
+    """Generate a salted hash of the input password.
+    Args:
+        password (str): The password to be hashed.
+    Returns:
+        bytes: The hashed password.
     """
-
-    def __init__(self):
-        self._db = DB()
-
-    def hash_password(password: str) -> bytes:
-        """Generate a salted hash of the input password.
-        Args:
-            password (str): The password to be hashed.
-        Returns:
-            bytes: The hashed password.
-        """
-        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
