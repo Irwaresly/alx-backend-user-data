a3bd52c4e9a78aee4abdcedfd65b0652f1420712
Irwaresly <silviairware372@gmail.com>
2024-06-13 05:02:19 -0700

    ua32

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,6 +62,18 @@ def login() -> str:
     return response
 
 
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """Log out
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
+
+
 # Run the Flask app if this script is executed directly
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
