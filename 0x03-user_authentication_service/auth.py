9c37590f64d51fcf643e95a3679b4350a50929e2
Irwaresly <silviairware372@gmail.com>
2024-06-13 01:57:33 -0700

    ua26

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,7 +15,6 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    @staticmethod
     def hash_password(password: str) -> bytes:
         """Generate a salted hash of the input password.
         Args:
@@ -24,27 +23,3 @@ class Auth:
             bytes: The hashed password.
         """
         return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
-    def register_user(self, email: str, password: str) -> User:
-        """
-        Register a new user.
-
-        Parameters:
-        email (str): User's email.
-        password (str): User's password.
-
-        Returns:
-        User: The new User object if successful.
-
-        Raises:
-        ValueError: If a user with the same email already exists.
-        """
-        try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists.")
-        except NoResultFound:
-            hashed_password = Auth.hash_password(password)
-            new_user = self._db.add_user(
-                email=email, hashed_password=hashed_password
-            )
-            return new_user
