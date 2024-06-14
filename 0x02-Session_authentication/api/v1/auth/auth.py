be9cd07f2ee1a813da9b321cdea1b7e1a8af819b
Irwaresly <silviairware372@gmail.com>
2024-06-14 02:20:38 -0700

    sa5

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -36,3 +36,11 @@ class Auth:
         """Gets the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request."""
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
